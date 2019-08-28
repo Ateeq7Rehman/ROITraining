dcf732b639d4deba91863163ab7a22f047adad6a
Kevin Rattan <kevin@time2yak.com>
2019-08-28 10:40:41 -0400

    initial commit

---
+++ ReadMe.md
@@ -0,0 +1,31 @@
+1. Getting started:
+
+prerequisites: node and npm
+    these are available inside GCP cloud shell if you can't install them locally.
+
+a. open two separate terminals: 1 in /server, 1 in /client
+b. run npm install in each terminal
+c. run npm start in each terminal
+d. navigate to http://localhost:8080
+
+2. The sample app
+
+Uses nodejs with the express web server on both server and client microservices.
+
+The backend server receives REST requests on port 8081 and returns mock data.
+
+The client inserts json from the server into an html template in the Views folder
+and returns it to the requestor on port 8080.
+
+client and server both use the following npm packages:
+
+express: a web server
+body-parser to convert json and form data in the request into parameters.
+mocha, chai and supertest (for unit testing - not included)
+
+The client uses the following additional libaries:
+
+request (sends REST requests)
+express-handlebars ( a templating library)
+
+
---
+++ server/server.js
@@ -0,0 +1,46 @@
+'use strict';
+
+// express is a nodejs web server
+// https://www.npmjs.com/package/express
+const express = require('express');
+
+// converts content in the request into parameter req.body
+// https://www.npmjs.com/package/body-parser
+const bodyParser = require('body-parser');
+
+// create the server
+const app = express();
+
+// the backend server will parse json, not a form request
+app.use(bodyParser.json()); 
+
+// health endpoint - returns an empty array
+app.get('/', (req, res) => {
+    res.json([]);
+});
+
+// mock events endpoint. this would be replaced by a call to a datastore
+// if you went on to develop this as a real application.
+app.get('/events', (req, res) => {
+    res.json([
+        { title: 'an event', id: 1234, description: 'something really cool' },
+        { title: 'another event', id: 5678, description: 'something even cooler' }
+    ]);
+});
+
+// create other methods here - version, login, add events etc
+
+
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ message: err.message });
+});
+
+const PORT = 8081;
+const server = app.listen(PORT, () => {
+    const host = server.address().address;
+    const port = server.address().port;
+
+    console.log(`Events app listening at http://${host}:${port}`);
+});
